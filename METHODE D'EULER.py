75d1a27902df994d6fe9ebf6c6319940e585a88f
Tony CALVEZ <tony.calvez@hotmail.fr>
2018-02-14 14:57:52 +0100

    Update METHODE D'EULER.py

--- METHODE D'EULER.py
+++ METHODE D'EULER.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+#CONDITION INITIALES
+X0=np.array([ 1, 0.0] )
+X =np.array([ 1, 0.0] ) # Les valeurs sont fausses
+Xp=np.array([ 1, 0.0] ) # Les valeurs sont fausses, oui c'est une variable en déclaration, hereusement que les valeurs ne sont pas rentrées dans le tableau. Merci Monsieur CANEVET.
+
 #EQUATION D'EVOLUTION
 def f(x,u):
     m=1.0
@@ -11,9 +16,10 @@ def f(x,u):
     return np.array(Xdot)
 
 #METHODE D'EULER
-def methode_euler(t,Xo):
-    for i in range( 0, 10 ):
-        X[i+1] = X[i] + h*f( X, u)
+def methode_euler(t,X0):
+    for i in range( 0, len(X)-1):
+        X[i+1] = X[i] + h*f( [ X[i], Xp[i]], X0[i])
+        Xp[i+1] = Xp[i] + h*X[i]
         
         
         
