3deeb4520f2f5d453489b7f16c06aad7142b8866
Tony CALVEZ <tony.calvez@hotmail.fr>
2018-02-23 10:35:31 +0100

    FIN

--- PENDULE.py
+++ PENDULE.py
@@ -6,7 +6,6 @@ def f(x,u):
     m=1.0
     g=9.81
     l=2.0
-    mu=0.1
     
     Xdot =   [x[1], -g*np.sin( x[0] )/l - mu*x[1]/(m*l**2) + u/(m*l**2) ]
     return np.array(Xdot)
@@ -57,18 +56,20 @@ if __name__ == "__main__":
     t=np.arange( 0.0, duree, h)
     print("taille: ", len(t),"duree: ", t[-1])
 
-    #METHODE EULER
-    X=methode_euler(t, X0, h)   
-    creationgraphique(t, X[1], 1, "Vitesse", "Temps", "Vitesse", "EULER")
-    creationgraphique(t, X[0], 2, "Position", "Temps", "Position", "EULER")
-    creationgraphique(X[0], X[1], 3, "Phase", "Distance", "Vitesse", "EULER")
-    
-    #METHODE RUNGE KUTTA
-    X=methode_RK(t, X0, h)   
-    creationgraphique(t, X[1], 1, "Vitesse", "Temps", "Vitesse", "RUNGE KUTTA")
-    creationgraphique(t, X[0], 2, "Position", "Temps", "Position", "RUNGE KUTTA")
-    creationgraphique(X[0], X[1], 3, "Phase", "Distance", "Vitesse", "RUNGE KUTTA")
-    
+    for i in range( 0, 10, 2 ) :
+        mu=i/10
+        #METHODE EULER
+        X=methode_euler(t, X0, h)   
+        creationgraphique(t, X[1], 1, "Vitesse avec la méthode d'EULER", "Temps", "Vitesse", mu)
+        creationgraphique(t, X[0], 2, "Position avec la méthode d'EULER", "Temps", "Position", mu)
+        creationgraphique(X[0], X[1], 3, "Phase avec la méthode d'EULER", "Distance", "Vitesse", mu)
+        
+        #METHODE RUNGE KUTTA
+        X=methode_RK(t, X0, h)   
+        creationgraphique(t, X[1], 4, "Vitesse  avec la méthode de RUNGE KUTTA", "Temps", "Vitesse",mu)
+        creationgraphique(t, X[0], 5, "Position  avec la méthode de RUNGE KUTTA", "Temps", "Position", mu)
+        creationgraphique(X[0], X[1], 6, "Phase  avec la méthode de RUNGE KUTTA", "Distance", "Vitesse", mu)
+        # mu élevé pour Euler et mu faible pour Runge Kuta
     
     #RESULTATS
 print(t,",",X0)
